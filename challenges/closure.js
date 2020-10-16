1d547f20f84975db751984bddf78d52545be3488
Kylevb405 <kylevanburen405@gmail.com>
2020-10-16 12:14:32 -0400

    finished closure & prototypes

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+/*
+The internal variable can be accessed by the nest function because of the lexical environment allowing the function to look outside of itself(in an outward direction) so to speak to look for that variable, if the variable was within another funtion not on the same scope or outer scope it would not be able to access the variable 
+*/
+
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+summation = function( number ) {
+  let summat = 0
+
+  for(let i = 1; i <= number; i++) {
+    summat = summat + i;
+  }
+  return summat
+}
